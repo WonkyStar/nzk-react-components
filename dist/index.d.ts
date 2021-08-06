cc56e58a03a8ed5429ab2fc0cfb43c38a1ebb406
Ben Afonso <ben@nightzookeeper.com>
2021-08-06 21:54:34 +0200

    build for next release

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+import Avatar from './components/Avatar';
 import Button from "./components/Button";
 import Icon from './components/Icon';
 import IconButton from './components/IconButton';
@@ -6,4 +7,4 @@ import { ModalProvider, useModalState } from './components/Modal/ModalState';
 import useAsync from './hooks/useAsync';
 import useConfettis from './hooks/useConfettis';
 import useMountState from './hooks/useMountState';
-export { Button, IconButton, Modal, Icon, ModalProvider, useModalState, useAsync, useMountState, useConfettis };
+export { Button, IconButton, Modal, Icon, Avatar, ModalProvider, useModalState, useAsync, useMountState, useConfettis };
