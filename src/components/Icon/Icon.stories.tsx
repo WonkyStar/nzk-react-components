9f59fd53d746ce0c39559860d8695f00b0ec919f
Ben Afonso <ben@nightzookeeper.com>
2021-08-07 02:27:34 +0200

    feat: add new icons

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,7 +1,10 @@
 import { Story } from "@storybook/react";
 import { Meta } from "@storybook/react/types-6-0";
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import useMountState from '../../hooks/useMountState';
 import Icon, { IconProps } from "./Icon";
+import ICONS from './icons';
 
 export default {
   title: "Components/Icon",
@@ -14,3 +17,83 @@ const Template: Story<IconProps> = (args) => <Icon {...args} />;
 // Reuse that template for creating different stories
 export const Primary = Template.bind({});
 Primary.args = { name: 'transfer' };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: -15px;
+
+  > :first-child {
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: 15px;
+    font-family: 'Rammetto One';
+  }
+  > :nth-child(2) {
+    margin-bottom: 15px;
+  }
+  > :nth-child(3) {
+    display: grid;
+    width: 100%;
+    background-color: #ebebeb;
+    grid-template-columns: repeat(auto-fill, 75px);
+    grid-gap: 20px;
+    padding: 15px;
+  }
+`
+
+const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 9px;
+  text-align: center;
+  cursor: pointer;
+  input {
+    height: 0;
+    width: 0;
+    opacity: 0;
+  }
+`
+
+export const AllIcons: Story = () => {
+  const [copied, setCopied] = useState<string | null>(null)
+  const mounted = useMountState()
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (copied && mounted) {
+        setCopied(null)
+      }
+    }, 3000)
+  }, [copied])
+
+  const displayedIcons = useMemo(() => {
+    return Object.keys(ICONS).filter(key => new RegExp(search).test(key))
+  }, [search])
+
+  return <Wrapper>
+    { copied
+      ? <div style={{ color: '#40A240' }}>Copied to clipboard: {copied}</div>
+      : <div>Click on an icon to copy its name</div>
+    }
+    <div>
+      <input type='text' placeholder='Search' value={search} onChange={(e) => setSearch(e.target.value)} />
+    </div>
+    <div>
+      {
+        displayedIcons.map(key => <IconWrapper key={key} onClick={() => {
+          setCopied(key)
+          const copyText: HTMLInputElement | null = document.querySelector(`#icon-${key}`);
+          copyText?.select();
+          document.execCommand("copy");
+        }}>
+          <input id={`icon-${key}`} defaultValue={key} />
+          <Icon key={key} name={key} />
+          <div>{key}</div>
+        </IconWrapper>)
+      }
+    </div>
+  </Wrapper>
+}
--- src/components/Icon/icons.tsx
+++ src/components/Icon/icons.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import { ReactComponent as AboutUs } from '../../assets/icons/about-us.svg'
 import { ReactComponent as Activities } from '../../assets/icons/activities.svg'
 import { ReactComponent as Archive } from '../../assets/icons/archive.svg'
 import { ReactComponent as ArrowLeft } from '../../assets/icons/arrow-left.svg'
@@ -10,33 +11,48 @@ import { ReactComponent as Checkmark } from '../../assets/icons/checkmark.svg'
 import { ReactComponent as Close } from '../../assets/icons/close copy.svg'
 import { ReactComponent as Collect } from '../../assets/icons/collect.svg'
 import { ReactComponent as Comment } from '../../assets/icons/comment.svg'
+import { ReactComponent as Compass } from '../../assets/icons/compass.svg'
 import { ReactComponent as Controller } from '../../assets/icons/controller.svg'
 import { ReactComponent as Create } from '../../assets/icons/create.svg'
 import { ReactComponent as Customise } from '../../assets/icons/customise.svg'
+import { ReactComponent as Download } from '../../assets/icons/download.svg'
 import { ReactComponent as Draft } from '../../assets/icons/draft.svg'
 import { ReactComponent as DrawingTask } from '../../assets/icons/drawing-task.svg'
 import { ReactComponent as Egg } from '../../assets/icons/egg.svg'
 import { ReactComponent as Enclosure } from '../../assets/icons/enclosure.svg'
+import { ReactComponent as ExclamationGrey } from '../../assets/icons/exclamation-grey.svg'
 import { ReactComponent as Exclamation } from '../../assets/icons/exclamation.svg'
 import { ReactComponent as Eye } from '../../assets/icons/eye.svg'
+import { ReactComponent as Facebook } from '../../assets/icons/facebook.svg'
 import { ReactComponent as Filter } from '../../assets/icons/filter.svg'
+import { ReactComponent as Flag } from '../../assets/icons/flag.svg'
 import { ReactComponent as ForYou } from '../../assets/icons/for-you.svg'
-import { ReactComponent as Games } from '../../assets/icons/games.svg'
+import { ReactComponent as Gift } from '../../assets/icons/gift.svg'
 import { ReactComponent as Goals } from '../../assets/icons/goals.svg'
 import { ReactComponent as Heart } from '../../assets/icons/heart.svg'
 import { ReactComponent as Help } from '../../assets/icons/help.svg'
 import { ReactComponent as Info } from '../../assets/icons/info.svg'
+import { ReactComponent as Instagram } from '../../assets/icons/instagram.svg'
 import { ReactComponent as Items } from '../../assets/icons/items.svg'
+import { ReactComponent as Layers } from '../../assets/icons/layers.svg'
 import { ReactComponent as Lessons } from '../../assets/icons/lessons.svg'
+import { ReactComponent as LinkIcon } from '../../assets/icons/link.svg'
+import { ReactComponent as List } from '../../assets/icons/list.svg'
 import { ReactComponent as Lock } from '../../assets/icons/lock.svg'
+import { ReactComponent as Logout } from '../../assets/icons/logout.svg'
+import { ReactComponent as Megaphone } from '../../assets/icons/megaphone.svg'
 import { ReactComponent as Menu } from '../../assets/icons/menu.svg'
 import { ReactComponent as Message } from '../../assets/icons/message.svg'
 import { ReactComponent as Minus } from '../../assets/icons/minus.svg'
+import { ReactComponent as More } from '../../assets/icons/more.svg'
 import { ReactComponent as MyHistory } from '../../assets/icons/my-history.svg'
 import { ReactComponent as MyZoo } from '../../assets/icons/my-zoo.svg'
 import { ReactComponent as New } from '../../assets/icons/new.svg'
 import { ReactComponent as Notifications } from '../../assets/icons/notifications.svg'
+import { ReactComponent as NZKPurple } from '../../assets/icons/NZK-purple.svg'
 import { ReactComponent as NZK } from '../../assets/icons/NZK.svg'
+import { ReactComponent as NZKTVBlue } from '../../assets/icons/nzktv-blue.svg'
+import { ReactComponent as NZKTVGreen } from '../../assets/icons/nzktv-green.svg'
 import { ReactComponent as OrbDisabled } from '../../assets/icons/orb-disabled.svg'
 import { ReactComponent as Orb } from '../../assets/icons/orb.svg'
 import { ReactComponent as Padlock } from '../../assets/icons/padlock.svg'
@@ -51,17 +67,26 @@ import { ReactComponent as Progress } from '../../assets/icons/progress.svg'
 import { ReactComponent as Projects } from '../../assets/icons/projects.svg'
 import { ReactComponent as Refresh } from '../../assets/icons/refresh.svg'
 import { ReactComponent as Rewards } from '../../assets/icons/rewards.svg'
+import { ReactComponent as Search } from '../../assets/icons/search.svg'
 import { ReactComponent as Settings } from '../../assets/icons/settings.svg'
 import { ReactComponent as Sharing } from '../../assets/icons/sharing.svg'
 import { ReactComponent as Shop } from '../../assets/icons/shop.svg'
 import { ReactComponent as ShoundOff } from '../../assets/icons/sound-off.svg'
 import { ReactComponent as SoundOn } from '../../assets/icons/sound-on.svg'
 import { ReactComponent as StarWriting } from '../../assets/icons/star-writing.svg'
-import { ReactComponent as Tick } from '../../assets/icons/tick.svg'
+import { ReactComponent as Task } from '../../assets/icons/task.svg'
+import { ReactComponent as TextToSpeech } from '../../assets/icons/text-to-speech.svg'
+import { ReactComponent as Torch } from '../../assets/icons/torch.svg'
 import { ReactComponent as Transfer } from '../../assets/icons/transfer.svg'
+import { ReactComponent as TrashWhite } from '../../assets/icons/trash-white.svg'
 import { ReactComponent as Trash } from '../../assets/icons/trash.svg'
-import { ReactComponent as Trophee } from '../../assets/icons/trophee.svg'
+import { ReactComponent as Trophy } from '../../assets/icons/trophy.svg'
+import { ReactComponent as Twitter } from '../../assets/icons/twitter.svg'
 import { ReactComponent as Upload } from '../../assets/icons/upload.svg'
+import { ReactComponent as User } from '../../assets/icons/user.svg'
+import { ReactComponent as Users } from '../../assets/icons/users.svg'
+import { ReactComponent as Video } from '../../assets/icons/video.svg'
+import { ReactComponent as Whatsapp } from '../../assets/icons/whatsapp.svg'
 import { ReactComponent as WritingTypeAdventureStory } from '../../assets/icons/writing-type-adventure-story.svg'
 import { ReactComponent as WritingTypeBiography } from '../../assets/icons/writing-type-biography.svg'
 import { ReactComponent as WritingTypeDiaryEntry } from '../../assets/icons/writing-type-diary-entry.svg'
@@ -99,7 +124,6 @@ export const ICONS: { [name: string]: ReactElement } = {
   'eye': <Eye />,
   'filter': <Filter />,
   'for-you': <ForYou />,
-  'games': <Games />,
   'goals': <Goals />,
   'heart': <Heart />,
   'help': <Help />,
@@ -110,7 +134,7 @@ export const ICONS: { [name: string]: ReactElement } = {
   'menu': <Menu />,
   'message': <Message />,
   'minus': <Minus />,
-  'my-history': <MyHistory />,
+  'book': <MyHistory />,
   'my-zoo': <MyZoo />,
   'new': <New />,
   'notifications': <Notifications />,
@@ -135,10 +159,9 @@ export const ICONS: { [name: string]: ReactElement } = {
   'sound-off': <ShoundOff />,
   'sound-on': <SoundOn />,
   'star-writing': <StarWriting />,
-  'tick': <Tick />,
   'transfer': <Transfer />,
   'trash': <Trash />,
-  'trophee': <Trophee />,
+  'trophy': <Trophy/>,
   'upload': <Upload />,
   'writing': <Writing />,
   'writing-type-adventure-story': <WritingTypeAdventureStory />,
@@ -152,7 +175,34 @@ export const ICONS: { [name: string]: ReactElement } = {
   'writing-type-playscript': <WritingTypePlayscript />,
   'writing-type-poem': <WritingTypePoem />,
   'writing-type-report': <WritingTypeReport />,
-  'zoo-point': <ZooPoint />
+  'zoo-point': <ZooPoint />,
+  'NZK-purple': <NZKPurple />,
+  'about-us': <AboutUs />,
+  'compass': <Compass />,
+  'download': <Download />,
+  'exclamation-grey': <ExclamationGrey />,
+  'facebook': <Facebook />,
+  'flag': <Flag />,
+  'gift': <Gift />,
+  'instagram': <Instagram />,
+  'layers': <Layers />,
+  'link': <LinkIcon />,
+  'list': <List />,
+  'logout': <Logout />,
+  'megaphone': <Megaphone />,
+  'more': <More />,
+  'nzktv-blue': <NZKTVBlue />,
+  'nzktv-green': <NZKTVGreen />,
+  'search': <Search />,
+  'task': <Task />,
+  'text-to-speech': <TextToSpeech />,
+  'torch': <Torch />,
+  'trash-white': <TrashWhite />,
+  'twitter': <Twitter />,
+  'user': <User />,
+  'users': <Users />,
+  'video': <Video />,
+  'whatsapp': <Whatsapp />,
 }
 
 export default ICONS
