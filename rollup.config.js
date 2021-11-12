9673a45ca44561fc43b52278444153473baf628a
Ben Afonso <ben@nightzookeeper.com>
2021-11-12 20:24:55 +0100

    feat: test rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,21 +5,23 @@ import svgr from "@svgr/rollup";
 import copy from "rollup-plugin-copy-assets";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
+import externals from "rollup-plugin-node-externals";
 
 export default {
   input: "src/index.ts",
   output: [
     {
       dir: "dist",
-      // exports: "auto",
+      exports: "auto",
       format: "cjs",
       sourcemap: true,
-      // preserveModules: true,
-      // preserveModulesRoot: "src",
+      preserveModules: true,
+      preserveModulesRoot: "src",
       // entryFileNames: "[name].js",
     },
   ],
   plugins: [
+    externals(),
     peerDepsExternal(),
     resolve({
       browser: true,
