9f444265b259771160c3142ef306488b6455d9bb
Ben Afonso <ben@nightzookeeper.com>
2024-04-20 02:50:41 +0200

    fix: transient props

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,8 +1,13 @@
-import styled, { css } from 'styled-components'
-import { BaseProps } from './Button'
-import { getPadding, getShadows, getSizeComparedToBase, getTextColor, getTextSize } from './utils'
-import { SIZES, THEMES } from './constants'
-
+import styled, { css } from "styled-components";
+import { BaseProps } from "./Button";
+import {
+  getPadding,
+  getShadows,
+  getSizeComparedToBase,
+  getTextColor,
+  getTextSize,
+} from "./utils";
+import { SIZES, THEMES } from "./constants";
 
 const getBaseStyle = () => {
   return css`
@@ -10,15 +15,19 @@ const getBaseStyle = () => {
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    font-family: 'Rammetto One';
+    font-family: "Rammetto One";
     border-radius: 1000px;
     cursor: pointer;
     user-select: none;
     text-decoration: none;
-  `
-}
+  `;
+};
 
-const getSizes = (props: { height: number, fontSize?: number, round?: boolean }) => {
+const getSizes = (props: {
+  height: number;
+  fontSize?: number;
+  round?: boolean;
+}) => {
   return css`
     outline: none;
     border: none;
@@ -26,53 +35,76 @@ const getSizes = (props: { height: number, fontSize?: number, round?: boolean })
     font-size: ${getTextSize(props)};
     margin-bottom: ${getSizeComparedToBase(props, 12)}px;
     padding: ${getPadding(props)};
-    ${props.round && css`
+    ${props.round &&
+    css`
       width: ${props.height}px;
       padding: 0;
       flex-shrink: 0;
     `}
-  `
-}
+  `;
+};
 
 const getColors = (props: BaseProps) => {
-  const theme = props.theme ? THEMES[props.theme] : undefined
-  const fallback = THEMES.primary
+  const theme = props.theme ? THEMES[props.theme] : undefined;
+  const fallback = THEMES.primary;
 
   return {
-    backgroundColor: theme?.backgroundColor
-      || props.backgroundColor
-      || fallback.backgroundColor,
+    backgroundColor:
+      theme?.backgroundColor ||
+      props.backgroundColor ||
+      fallback.backgroundColor,
     color: theme?.color || props.color,
     strokeColor: theme?.strokeColor || props.strokeColor,
     shadowColor: theme?.shadowColor || props.shadowColor,
-  }
-}
-
+  };
+};
 
 const getHeight = (props: BaseProps) => {
-  const size = props.size ? SIZES[props.size] : undefined
-  return size || props.height || SIZES.regular
-}
+  const size = props.size ? SIZES[props.size] : undefined;
+  return size || props.height || SIZES.regular;
+};
 
-export const StyledSpanButton = styled.div.attrs(props => ({
-  role: 'button' || props.role
-}))<BaseProps>`
-  ${props => {
-    const colors = getColors(props)
-    const height = getHeight(props)
-  
+const buttonProps = [
+  "backgroundColor",
+  "height",
+  "fullWidth",
+  "color",
+  "strokeColor",
+  "dropShadowColor",
+  "shadowColor",
+  "fontSize",
+  "round",
+  "disabled",
+  "wiggle",
+  "size",
+  "theme",
+];
+
+export const StyledSpanButton = styled.div
+  .withConfig({
+    shouldForwardProp: (prop) => {
+      return !buttonProps.includes(prop);
+    },
+  })
+  .attrs((props) => ({
+    role: "button" || props.role,
+  }))<BaseProps>`
+  ${(props) => {
+    const colors = getColors(props);
+    const height = getHeight(props);
+
     return css`
       // Base
       ${getBaseStyle()}
       // COLORS
       background-color: ${colors.backgroundColor};
       ${getShadows({ ...colors, height })};
-      color: ${getTextColor(colors) };
-      filter: ${props.disabled ? 'grayscale(100%)' : 'none'};
+      color: ${getTextColor(colors)};
+      filter: ${props.disabled ? "grayscale(100%)" : "none"};
 
       // Dimensions
       ${getSizes({ height, round: props.round })}
-    `
+    `;
   }}
 
   svg {
@@ -80,4 +112,4 @@ export const StyledSpanButton = styled.div.attrs(props => ({
     width: 100%;
   }
 
-`
+`;
