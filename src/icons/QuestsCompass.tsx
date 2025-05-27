f800a415c991c91e9058e116f85a5c91a3acd810
Ben Afonso <ben@nightzookeeper.com>
2025-05-27 22:53:59 +0200

    chore: add new icons
    dens and quests compass

---
+++ src/icons/QuestsCompass.tsx
@@ -0,0 +1,185 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgQuestsCompass = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 70 70"
+    {...props}
+  >
+    <defs>
+      <clipPath id="quests-compass_svg__a">
+        <path
+          fill="none"
+          d="M0 0h45.069v46.442H0z"
+          data-name="Rectangle 2736"
+        />
+      </clipPath>
+      <clipPath id="quests-compass_svg__b">
+        <path
+          fill="none"
+          d="M0 0h17.909v18.558H0z"
+          data-name="Rectangle 2737"
+        />
+      </clipPath>
+      <clipPath id="quests-compass_svg__c">
+        <path fill="none" d="M0 0h9.664v9.056H0z" data-name="Rectangle 2738" />
+      </clipPath>
+      <clipPath id="quests-compass_svg__d">
+        <path fill="none" d="M0 0h7.679v9.226H0z" data-name="Rectangle 2739" />
+      </clipPath>
+      <clipPath id="quests-compass_svg__e">
+        <path fill="none" d="M0 0h19.57v20.124H0z" data-name="Rectangle 2740" />
+      </clipPath>
+    </defs>
+    <g data-name="Group 3847">
+      <g data-name="Group 3850">
+        <g data-name="Group 3830" opacity={0.3}>
+          <g data-name="Group 3829">
+            <g
+              clipPath="url(#quests-compass_svg__a)"
+              data-name="Group 3828"
+              transform="translate(13.264 17.25)"
+            >
+              <path
+                d="m42.37 34.667-1.632-1.209a15.1 15.1 0 0 0 3.141-5.5 17.85 17.85 0 0 0-1.51-15.445 23.74 23.74 0 0 0-13.119-10.9 23.77 23.77 0 0 0-17.08-.057 18.01 18.01 0 0 0-9.551 26.33 23.78 23.78 0 0 0 13.142 10.909 23.1 23.1 0 0 0 11.928 1.436l1.182 6.21 16.2-9.78v-3.623Zm-10.7-9.088a10.14 10.14 0 0 1-13.011 6.085 10.14 10.14 0 0 1-6.082-13.013 10.14 10.14 0 0 1 13.011-6.084 10.14 10.14 0 0 1 6.084 13.012"
+                data-name="Path 6370"
+              />
+            </g>
+          </g>
+        </g>
+        <path
+          fill="#afafaf"
+          d="m55.634 48.293-1.632-1.208a15.1 15.1 0 0 0 3.141-5.5 17.85 17.85 0 0 0-1.51-15.445 23.74 23.74 0 0 0-13.119-10.9 23.77 23.77 0 0 0-17.08-.057 18.01 18.01 0 0 0-9.551 26.33A23.77 23.77 0 0 0 29.025 52.42a23.1 23.1 0 0 0 11.929 1.436l1.182 6.21 16.2-9.779v-3.623Zm-10.7-10.627a10.14 10.14 0 0 1-13.011 6.085 10.14 10.14 0 0 1-6.085-13.012 10.14 10.14 0 0 1 13.014-6.085 10.14 10.14 0 0 1 6.084 13.012"
+          data-name="Path 6371"
+        />
+        <path
+          fill="#fff"
+          d="m54.002 43.461 4.331 3.208-16.2 9.78-1.181-6.21a23.1 23.1 0 0 1-11.929-1.436A23.78 23.78 0 0 1 15.88 37.891a18.01 18.01 0 0 1 9.551-26.33 23.78 23.78 0 0 1 17.081.057 23.74 23.74 0 0 1 13.119 10.9 17.85 17.85 0 0 1 1.51 15.445 15.1 15.1 0 0 1-3.14 5.5m-14.307-5.216a9.8 9.8 0 0 0 5.243-5.743 9.8 9.8 0 0 0-.342-7.769 10.18 10.18 0 0 0-13.511-4.9 9.8 9.8 0 0 0-5.243 5.743 9.8 9.8 0 0 0 .342 7.769 10.18 10.18 0 0 0 13.511 4.9"
+          data-name="Path 6372"
+        />
+        <path
+          fill="#e57200"
+          d="M39.953 39.799a10.18 10.18 0 0 0 4.9-13.511 10.18 10.18 0 0 0-13.511-4.9 10.18 10.18 0 0 0-4.9 13.511 10.18 10.18 0 0 0 13.511 4.9"
+          data-name="Path 6373"
+        />
+        <path
+          fill="#fcee21"
+          d="M39.694 38.246a10.18 10.18 0 0 0 4.9-13.511 10.18 10.18 0 0 0-13.511-4.9 10.18 10.18 0 0 0-4.9 13.511 10.18 10.18 0 0 0 13.511 4.9"
+          data-name="Path 6374"
+        />
+        <path
+          fill="#f7931e"
+          d="M39.643 38.385a9.34 9.34 0 0 0 4.654-12.841 10.56 10.56 0 0 0-13.511-4.9 9.337 9.337 0 0 0-4.658 12.841 10.56 10.56 0 0 0 13.511 4.9"
+          data-name="Path 6375"
+        />
+        <path
+          fill="#fbb03b"
+          d="M39.983 37.45a8.42 8.42 0 0 0 4.377-12.067 11.06 11.06 0 0 0-13.511-4.9 8.42 8.42 0 0 0-4.377 12.067 11.06 11.06 0 0 0 13.511 4.9"
+          data-name="Path 6376"
+        />
+        <path
+          fill="red"
+          d="m27.496 18.975 2.861 9.651 15.046 8.906-4.157-13.13Z"
+          data-name="Path 6377"
+        />
+        <path
+          fill="#ff488e"
+          d="m27.496 18.975 8.592 5.427 9.315 13.13-4.157-13.13Z"
+          data-name="Path 6378"
+        />
+        <path
+          fill="#00a4ff"
+          d="M41.246 24.402h-5.157l-5.732 4.223 15.051 8.908Z"
+          data-name="Path 6379"
+        />
+        <g data-name="Group 3833" opacity={0.3}>
+          <g data-name="Group 3832">
+            <g
+              clipPath="url(#quests-compass_svg__b)"
+              data-name="Group 3831"
+              transform="translate(27.496 18.975)"
+            >
+              <path
+                d="m0 0 2.861 9.651 15.046 8.906-13.1-10.351Z"
+                data-name="Path 6380"
+              />
+            </g>
+          </g>
+        </g>
+        <g data-name="Group 3836" opacity={0.26}>
+          <g data-name="Group 3835">
+            <g
+              clipPath="url(#quests-compass_svg__c)"
+              data-name="Group 3834"
+              transform="translate(21.559 12.835)"
+            >
+              <path
+                fill="#1b1464"
+                d="m4.241 5.037 1.465 2.582-1.384 1.437L0 3.884l1.913-1.935 3.415 1.074-1.617-1.819 3.085-1.2 2.868 5.164-1.73 1.181Z"
+                data-name="Path 6381"
+              />
+            </g>
+          </g>
+        </g>
+        <g data-name="Group 3839" opacity={0.26}>
+          <g data-name="Group 3838">
+            <g
+              clipPath="url(#quests-compass_svg__d)"
+              data-name="Group 3837"
+              transform="translate(43.545 40.137)"
+            >
+              <path
+                fill="#1b1464"
+                d="M2.499 5.976A1.73 1.73 0 0 1 .248 4.812a4 4 0 0 1-.16-.446Q-.57 2.273 3.688.001l1.519 2.252-.45.189q-1.689.733-1.637 1.1a.1.1 0 0 0 .011.051q.144.331 1.772-.286 1.691-.638 2.587 1.429l.043.1q.9 2.189-3.8 4.4L1.664 7.084a18 18 0 0 0 1.784-.629q.779-.338.789-.558a.2.2 0 0 0-.015-.06q-.125-.289-1.726.149"
+                data-name="Path 6382"
+              />
+            </g>
+          </g>
+        </g>
+        <path
+          fill="#afafaf"
+          d="M44.831 35.723q.198-.428.364-.885a10.1 10.1 0 0 0 .633-3.932 10.4 10.4 0 0 1-.633 3.156 11 11 0 0 1-.513 1.192Z"
+          data-name="Path 6383"
+        />
+        <g data-name="Group 3842" opacity={0.3}>
+          <g data-name="Group 3841">
+            <g
+              clipPath="url(#quests-compass_svg__e)"
+              data-name="Group 3840"
+              transform="translate(25.46 21.437)"
+            >
+              <path
+                d="m19.57 18.396-1.442-2.092a10 10 0 0 0 .642-.9l-.371-.22c-.174.242-.363.472-.558.7l.109-.962L4.897 7.189 2.909.489 2.393.74 1.519 0l.243 1.515.321-.251 2.436 8.224 10.593 6.27.629.372.255 1.378a11 11 0 0 1-1.5.856A10.18 10.18 0 0 1 .981 13.458a10.1 10.1 0 0 1-.975-3.835 9.9 9.9 0 0 0 .975 4.612 10.18 10.18 0 0 0 13.511 4.9 10.4 10.4 0 0 0 2.273-1.437Z"
+                data-name="Path 6384"
+              />
+            </g>
+          </g>
+        </g>
+        <path
+          fill="#69d1ff"
+          d="m36.088 24.402 9.316 13.131-4.158-13.131Z"
+          data-name="Path 6385"
+        />
+        <g data-name="Group 3845" opacity={0.6}>
+          <g data-name="Group 3844">
+            <g
+              clipPath="url(#quests-compass_svg__b)"
+              data-name="Group 3843"
+              transform="translate(27.496 18.975)"
+            >
+              <path
+                fill="#fff"
+                d="m0 0 12.273 5.437 5.634 13.12-4.157-13.13Z"
+                data-name="Path 6386"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+    <path fill="none" d="M0 0h70v70H0z" />
+  </svg>
+);
+export default SvgQuestsCompass;
