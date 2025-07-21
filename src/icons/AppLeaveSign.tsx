8d7698341b10f1df604f3064990906e9a6d02858
Ben Afonso <ben@nightzookeeper.com>
2025-07-21 11:51:52 +0200

    chore: new icons

---
+++ src/icons/AppLeaveSign.tsx
@@ -0,0 +1,120 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgAppLeaveSign = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 70 70"
+    {...props}
+  >
+    <defs>
+      <clipPath id="app-leave-sign_svg__a">
+        <path fill="none" d="M0 0h70v70H0z" data-name="Rectangle 5269" />
+      </clipPath>
+      <clipPath id="app-leave-sign_svg__b">
+        <path
+          fill="none"
+          d="M.207 13.253H70V58.61H.207z"
+          data-name="Rectangle 5266"
+        />
+      </clipPath>
+      <clipPath id="app-leave-sign_svg__c">
+        <path
+          fill="none"
+          d="M.208 13.253H70v45.356H.208z"
+          data-name="Rectangle 5265"
+        />
+      </clipPath>
+      <clipPath id="app-leave-sign_svg__d">
+        <path
+          fill="none"
+          d="M5.697 18.084H60.12v25.661H5.697z"
+          data-name="Rectangle 5267"
+        />
+      </clipPath>
+    </defs>
+    <g clipPath="url(#app-leave-sign_svg__a)" data-name="Group 14819">
+      <g data-name="Group 14818">
+        <g clipPath="url(#app-leave-sign_svg__a)" data-name="Group 14817">
+          <g
+            data-name="Group 14813"
+            opacity={0.3}
+            style={{
+              mixBlendMode: "normal",
+              isolation: "isolate",
+            }}
+          >
+            <g data-name="Group 14812">
+              <g clipPath="url(#app-leave-sign_svg__b)" data-name="Group 14811">
+                <g data-name="Group 14810">
+                  <g
+                    clipPath="url(#app-leave-sign_svg__c)"
+                    data-name="Group 14809"
+                  >
+                    <path
+                      d="M69.689 32.818c-.673-2-20.856-18.729-23.638-19.522-2.023-.577-1.545 4.9-3.1 5.348S.438 15.638.221 18.083c-.027.314-.014 1.108.038 2.25a.2.2 0 0 0-.031.087C.011 22.87 2.105 58.357 5 58.57c.28.243 4.419-.659 10.366-2.059 3.725-.87 8.008-1.907 12.109-2.892h.01c11.766-2.812 16.437-3.491 16.437-3.491 2 .44 2.907 5.778 4.687 5.775a1.4 1.4 0 0 0 .5-.169c3.591-1.776 21.5-16.88 20.869-18.764-.075-.224.229-3.562-.292-4.152"
+                      data-name="Path 12679"
+                    />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+          <path
+            fill="#bdbdbd"
+            d="M46.057 11.655c2.47.7 19.518 12.5 23.632 17.186.518.59.214 3.928.292 4.152.67 2-19.593 18.93-21.373 18.934s-2.683-5.334-4.688-5.775S7.892 54.813 5 54.6.011 18.9.228 16.45s41.171 1.007 42.727.558 1.08-5.925 3.1-5.348"
+            data-name="Path 12680"
+          />
+          <path
+            fill="#fff"
+            d="M46.051 9.319c2.782.793 22.966 17.522 23.638 19.522S50.382 49.584 48.6 49.591s-2.683-5.334-4.688-5.775S8.1 48.581 5.2 48.366.006 16.556.222 14.108s41.171 1.008 42.727.558 1.08-5.925 3.1-5.348"
+            data-name="Path 12681"
+          />
+          <path
+            fill="#8a8a8a"
+            d="M43.919 46.153c.834-.225.1-2.414-.844-2.35s-22.843 2.992-30.028 3.773S4.55 54.209 5 54.6s10.72-2.142 22.485-4.953 16.436-3.494 16.436-3.494"
+            data-name="Path 12682"
+          />
+          <path
+            fill="#8a8a8a"
+            d="M69.718 33.806s-.968-2.284-6.625 3.177-13.16 11.658-13.768 12.606a2.86 2.86 0 0 0-.223 2.17s12.867-8.851 20.616-17.953"
+            data-name="Path 12683"
+          />
+          <g
+            data-name="Group 14816"
+            style={{
+              mixBlendMode: "normal",
+              isolation: "isolate",
+            }}
+          >
+            <g data-name="Group 14815">
+              <g clipPath="url(#app-leave-sign_svg__d)" data-name="Group 14814">
+                <text
+                  fill="#662d80"
+                  data-name="Leave"
+                  fontFamily="RammettoOne-Regular, Rammetto One"
+                  fontSize={14.583}
+                  transform="rotate(-6.024 350.447 -48.834)"
+                >
+                  <tspan x={0} y={0}>
+                    {"L"}
+                  </tspan>
+                  <tspan y={0} letterSpacing="-.021em">
+                    {"e"}
+                  </tspan>
+                  <tspan y={0}>{"a"}</tspan>
+                  <tspan y={0} letterSpacing="-.043em">
+                    {"v"}
+                  </tspan>
+                  <tspan y={0}>{"e"}</tspan>
+                </text>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default SvgAppLeaveSign;
---
+++ src/icons/AppPencil.tsx
@@ -0,0 +1,489 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgAppPencil = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="1em"
+    height="1em"
+    viewBox="0 0 70 70"
+    {...props}
+  >
+    <defs>
+      <clipPath id="app-pencil_svg__b">
+        <path fill="none" d="M0 0h70v70H0z" data-name="Rectangle 2929" />
+      </clipPath>
+      <clipPath id="app-pencil_svg__c">
+        <path
+          fill="none"
+          d="M14.028 13.815h55.141v51.379H14.028z"
+          data-name="Rectangle 2911"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__d">
+        <path
+          fill="none"
+          d="M46.63 0h17.917v9.285H46.63z"
+          data-name="Rectangle 2912"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__e">
+        <path
+          fill="none"
+          d="M53.972 5.564h11.969v12.931H53.972z"
+          data-name="Rectangle 2913"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__f">
+        <path
+          fill="none"
+          d="M50.629 9.167h12.186v14.668H50.629z"
+          data-name="Rectangle 2914"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__g">
+        <path
+          fill="none"
+          d="M36.41 10.7a312 312 0 0 0-19.789 30.982S14.852 51.6 14.506 53.84s-1.206 7.922-.784 8.967 4.671 2.431 4.671 2.431 1.134-1.262 4.514-2.527c3.944-1.478 16.685-7.976 16.685-7.976a60632 60632 0 0 1 19.623-30.808c-4.35-11.758-12.929-13.746-18.235-13.746a18.6 18.6 0 0 0-4.57.52"
+          data-name="Path 5130"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__i">
+        <path
+          fill="none"
+          d="m16.621 41.682-2.915 16.752s5.876.389 6.742 1.2.65 4.2.65 4.2l18.494-9.1s-1.217-2.73-2.868-3.469-4.924.035-4.924.035 1.506-6.452-.347-7.6-7.637 1.923-7.637 1.923-1.073-3.273-2.061-3.609a30.7 30.7 0 0 0-5.1-.328h-.031"
+          data-name="Path 5131"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__k">
+        <path
+          fill="none"
+          d="M21.097 14.369h38.118V63.84H21.097z"
+          data-name="Rectangle 2918"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__l">
+        <path
+          fill="none"
+          d="M52.883 14.369S37.856 34.623 31.8 51.3L21.1 63.84l18.495-9.1s6.21-8.616 19.623-30.807c0 0-2.281-7-6.332-9.559"
+          data-name="Path 5133"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__m">
+        <path
+          fill="none"
+          d="M23.863 11.114h21.123v35.87H23.863z"
+          data-name="Rectangle 2919"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__n">
+        <path
+          fill="none"
+          d="M17.42 43.013h12.482v14.696H17.42z"
+          data-name="Rectangle 2920"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__o">
+        <path
+          fill="none"
+          d="M12.989 61.886h5.232v3.698h-5.232z"
+          data-name="Rectangle 2921"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__p">
+        <path
+          fill="none"
+          d="M41.885 15.913h15.216v31.102H41.885z"
+          data-name="Rectangle 2923"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__q">
+        <path
+          fill="none"
+          d="M56.552 27.2c1.919-2.762-1.352-19.353-7.226-6.4a100.2 100.2 0 0 0-7.441 26.22Z"
+          data-name="Path 5139"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__s">
+        <path
+          fill="none"
+          d="M31.802 50.183h7.789v4.552h-7.789z"
+          data-name="Rectangle 2924"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__t">
+        <path
+          fill="none"
+          d="M38.841 6.142h22.084v14.93H38.841z"
+          data-name="Rectangle 2926"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__u">
+        <path
+          fill="none"
+          d="M38.841 7.4s6.311-.708 10.415 1.7c3.852 2.263 9.049 6.183 11.272 11.01q.218.474.4.959s-1.128-7.12-9.496-12.213S38.841 7.4 38.841 7.4"
+          data-name="Path 5141"
+        />
+      </clipPath>
+      <clipPath id="app-pencil_svg__w">
+        <path
+          fill="none"
+          d="M16.621 10.701h19.788v30.981H16.621z"
+          data-name="Rectangle 2927"
+        />
+      </clipPath>
+      <linearGradient
+        id="app-pencil_svg__j"
+        x1={-0.585}
+        x2={-0.544}
+        y1={1.118}
+        y2={1.118}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#f9b48d" />
+        <stop offset={0.472} stopColor="#f7af8b" />
+        <stop offset={0.996} stopColor="#f5a289" />
+        <stop offset={1} stopColor="#f5a289" />
+      </linearGradient>
+      <linearGradient
+        id="app-pencil_svg__a"
+        x1={-0.174}
+        x2={-0.157}
+        y1={1.085}
+        y2={1.085}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#b63561" />
+        <stop offset={0.185} stopColor="#b1335f" />
+        <stop offset={0.392} stopColor="#a32e5b" />
+        <stop offset={0.609} stopColor="#8d2654" />
+        <stop offset={0.832} stopColor="#6e1b4a" />
+        <stop offset={1} stopColor="#521142" />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#app-pencil_svg__a"
+        id="app-pencil_svg__r"
+        x1={-0.681}
+        x2={-0.657}
+        y1={1.978}
+        y2={1.978}
+      />
+      <linearGradient
+        id="app-pencil_svg__v"
+        x1={-1.375}
+        x2={-1.338}
+        y1={2.934}
+        y2={2.934}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#727171" />
+        <stop offset={0.194} stopColor="#6d6c6c" />
+        <stop offset={0.41} stopColor="#605f5f" />
+        <stop offset={0.636} stopColor="#494948" />
+        <stop offset={0.868} stopColor="#2b2a2a" />
+        <stop offset={1} stopColor="#161615" />
+      </linearGradient>
+      <radialGradient
+        id="app-pencil_svg__h"
+        cx={0.626}
+        cy={0.439}
+        r={0.486}
+        gradientUnits="objectBoundingBox"
+      >
+        <stop offset={0} stopColor="#ed690a" />
+        <stop offset={0.545} stopColor="#e86706" />
+        <stop offset={1} stopColor="#df6400" />
+      </radialGradient>
+    </defs>
+    <g data-name="Group 7536">
+      <g clipPath="url(#app-pencil_svg__b)" data-name="Group 7535">
+        <g data-name="Group 7498">
+          <g clipPath="url(#app-pencil_svg__b)" data-name="Group 7497">
+            <g data-name="Group 7487" opacity={0.3}>
+              <g data-name="Group 7486">
+                <g clipPath="url(#app-pencil_svg__c)" data-name="Group 7485">
+                  <path
+                    fill="#161615"
+                    d="M68.539 30.872a4.8 4.8 0 0 0 .438-2.194l.02-.071a6.35 6.35 0 0 0-.524-4.182 15 15 0 0 0-.8-1.565 27 27 0 0 0-1.051-1.644q-.258-.375-.519-.747l-.055-.078q-.187-.267-.381-.535l-.139-.191-.16-.219a27 27 0 0 0-2.241-2.722q-.094-.098-.188-.2c-.094-.102-.12-.122-.181-.181q-.21-.208-.423-.4l-.022-.021-.013-.012-.048-.043-.029-.026a11 11 0 0 0-1.13-.888c-.978-.7-2.181-1.369-2.749-1.062a5.3 5.3 0 0 0-3.489 1.137c-.364.259-.866.55-1.356.884a2.4 2.4 0 0 0-2.548 1.177c-.206.436-.311 1.018-.71 1.187-.283.119-.652 0-.969.033a2.63 2.63 0 0 0-1.618 1.831 4 4 0 0 0-.044.489l-.19-.007a310 310 0 0 0-24.213 22.792s-4.333 8.451-5.272 10.375l-1.993 3.92a40 40 0 0 0-1.914 3.891c.034.291 1.915 3.39 2.813 3.562a.4.4 0 0 0 .237.024 7.2 7.2 0 0 0 2.512-.648 36 36 0 0 1 1.942-.238c3.4-.328 14.91-3.359 14.91-3.359s7.263-6.159 24.037-22.678c0 0 .009-.156.017-.432l.744-.512.746-.515q.094-.063.187-.133a1.4 1.4 0 0 0 .4-.431 1 1 0 0 0 .052-.107 2 2 0 0 0 .075-.233 2.3 2.3 0 0 1 .19-.595l.02-.035a1 1 0 0 1 .061-.075 1 1 0 0 1 .069-.061 1.9 1.9 0 0 1 .916-.2q.06-.007.117-.017a1 1 0 0 0 .161-.038 1 1 0 0 0 .112-.042 1 1 0 0 0 .111-.063 1 1 0 0 0 .139-.123 1 1 0 0 0 .123-.158A2.5 2.5 0 0 0 65 33.344c.347-.241.745-.482 1.149-.722l.163-.1.327-.194c.267-.16.53-.32.773-.48q.148-.097.283-.193a4.2 4.2 0 0 0 .729-.631l.043-.05a1 1 0 0 0 .071-.106"
+                    data-name="Path 5123"
+                  />
+                </g>
+              </g>
+            </g>
+            <path
+              fill="#d6006c"
+              d="M43.056 2.591a6.6 6.6 0 0 1 4.4-2.423c3.5-.363 7.676 1.853 11.2 4a28 28 0 0 1 4.534 3.218c1.313 1.222 2.3 2.7 2.324 4 .01.713.6 1.939.326 2.549-.588 1.332-3.324 3.226-3.912 4.555-2.511-1.132-3.854-2.938-6.438-3.78a55.6 55.6 0 0 1-11.747-5.44c-1.456-.894-3.194-1.8-3.163-3.138.03-1.279 1.684-2.632 2.476-3.545"
+              data-name="Path 5124"
+            />
+            <g
+              data-name="Group 7490"
+              opacity={0.26}
+              style={{
+                mixBlendMode: "normal",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7489">
+                <g clipPath="url(#app-pencil_svg__d)" data-name="Group 7488">
+                  <path
+                    fill="#fff"
+                    d="M46.63.48c.439-1.123 4.432.014 5.734.486a35.8 35.8 0 0 1 6.289 3.2 28 28 0 0 1 4.537 3.219 13 13 0 0 1 1.252 1.489c.152.185.151.458-.084.4a.7.7 0 0 1-.225-.118c-2.319-1.616-4.353-3.821-6.956-4.691a3.3 3.3 0 0 0-.914-.192 3.6 3.6 0 0 0-.891.144 5.37 5.37 0 0 1-3.262-.4 4.4 4.4 0 0 1-2.424-2.4A8.7 8.7 0 0 1 46.63.48"
+                    data-name="Path 5125"
+                  />
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7493"
+              opacity={0.36}
+              style={{
+                mixBlendMode: "normal",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7492">
+                <g clipPath="url(#app-pencil_svg__e)" data-name="Group 7491">
+                  <path
+                    fill="#292e43"
+                    d="M61.2 18.129q.348.191.729.365c.588-1.33 3.324-3.224 3.912-4.554.4-.894-.431-3.211-1.317-4.27a9.7 9.7 0 0 0-1.754-1.463 28.4 28.4 0 0 1-3.128-2.643c-1.44.664-2.035 2.007-2.845 3.184q-1.422 2.065-2.823 4.145 3.744 2.471 7.226 5.236"
+                    data-name="Path 5126"
+                  />
+                </g>
+              </g>
+            </g>
+            <path
+              fill="#8b8a88"
+              d="M37.693 7.761A5.3 5.3 0 0 0 38.84 7.4c.41-.3.3-.921.37-1.427.161-1.164 1.351-1.8 2.7-2.064 3.5-.685 8.281.765 12.106 3.316a24.04 24.04 0 0 1 8.322 9.5c.432.919.762 1.971.129 2.475-.458.366-1.362.376-1.546.917a4.4 4.4 0 0 0 .007.954 1.56 1.56 0 0 1-.493.887l-1.68 1.876A78 78 0 0 0 43.358 13.42a13 13 0 0 0-3.746-1.432c-1.938-.3-2.748-.186-3.118-1.848a2.59 2.59 0 0 1 1.2-2.379"
+              data-name="Path 5127"
+            />
+            <g
+              data-name="Group 7496"
+              opacity={0.36}
+              style={{
+                mixBlendMode: "normal",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7495">
+                <g clipPath="url(#app-pencil_svg__f)" data-name="Group 7494">
+                  <path
+                    fill="#292e43"
+                    d="m58.752 23.835 1.679-1.876a1.56 1.56 0 0 0 .494-.887 4.4 4.4 0 0 1-.008-.954c.184-.542 1.088-.551 1.546-.917.633-.5.3-1.556-.128-2.475a23.6 23.6 0 0 0-5.794-7.559 348 348 0 0 0-5.912 8.51 77 77 0 0 1 8.123 6.158"
+                    data-name="Path 5128"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+        <g data-name="Group 7500">
+          <g clipPath="url(#app-pencil_svg__g)" data-name="Group 7499">
+            <path
+              fill="url(#app-pencil_svg__h)"
+              d="M71.913 15.866 18.041-8.253.6 59.553l53.874 24.119Z"
+              data-name="Path 5129"
+            />
+          </g>
+        </g>
+        <g data-name="Group 7502">
+          <g clipPath="url(#app-pencil_svg__i)" data-name="Group 7501">
+            <path
+              fill="url(#app-pencil_svg__j)"
+              d="M0 0h24.567v27.914H0z"
+              data-name="Rectangle 2916"
+              transform="rotate(-84.431 40.852 25.441)"
+            />
+          </g>
+        </g>
+        <g data-name="Group 7534">
+          <g clipPath="url(#app-pencil_svg__b)" data-name="Group 7533">
+            <path
+              fill="#272b39"
+              d="M21.1 63.84s.513-3.949-.962-4.929-6.428-.476-6.428-.476a35 35 0 0 0-.719 4.538c.16.279 3.67 2.743 4.816 2.611A9.3 9.3 0 0 0 21.1 63.84"
+              data-name="Path 5132"
+            />
+            <g
+              data-name="Group 7507"
+              opacity={0.4}
+              style={{
+                mixBlendMode: "multiply",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7506">
+                <g clipPath="url(#app-pencil_svg__k)" data-name="Group 7505">
+                  <g data-name="Group 7504">
+                    <g
+                      clipPath="url(#app-pencil_svg__l)"
+                      data-name="Group 7503"
+                    >
+                      <path
+                        fill="url(#app-pencil_svg__a)"
+                        d="M0 0h59.619v52.907H0z"
+                        data-name="Rectangle 2917"
+                        transform="rotate(-69.712 44.065 25.35)"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7510"
+              opacity={0.61}
+              style={{
+                mixBlendMode: "soft-light",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7509">
+                <g clipPath="url(#app-pencil_svg__m)" data-name="Group 7508">
+                  <path
+                    fill="#fff6bb"
+                    d="M44.985 11.114 23.862 42.683s5.362-.461 7.107-.085 2.679 4.386 2.679 4.386l1.345-6.512s-2.731-.758-2.408-5.3 12.4-24.058 12.4-24.058"
+                    data-name="Path 5134"
+                  />
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7513"
+              opacity={0.61}
+              style={{
+                mixBlendMode: "soft-light",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7512">
+                <g clipPath="url(#app-pencil_svg__n)" data-name="Group 7511">
+                  <path
+                    fill="#fff6bb"
+                    d="M21.343 43.02c-1.643.152-6.837 10.82-1.732 14.325 2.61 1.792 10.442-3.42 10.288-7s-6.082-4.722-6.082-4.722-.744-2.759-2.474-2.6"
+                    data-name="Path 5135"
+                  />
+                </g>
+              </g>
+            </g>
+            <path
+              fill="#f7bebc"
+              d="M40.067 18.273s-11.239 17.48-14.524 22.494 1.933-3.345 1.933-3.345Z"
+              data-name="Path 5136"
+            />
+            <path
+              fill="#f7bebc"
+              d="M33.694 14.85S21.782 34 18.3 39.5s2.057-3.659 2.057-3.659Z"
+              data-name="Path 5137"
+            />
+            <g
+              data-name="Group 7516"
+              opacity={0.56}
+              style={{
+                mixBlendMode: "multiply",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7515">
+                <g clipPath="url(#app-pencil_svg__o)" data-name="Group 7514">
+                  <path
+                    fill="#560f41"
+                    d="M17.805 65.584c1.716 0-2.367-3.184-3.215-3.617s-1.6 1.006-1.6 1.006 3.4 2.608 4.816 2.611"
+                    data-name="Path 5138"
+                  />
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7521"
+              opacity={0.4}
+              style={{
+                mixBlendMode: "multiply",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7520">
+                <g clipPath="url(#app-pencil_svg__p)" data-name="Group 7519">
+                  <g data-name="Group 7518">
+                    <g
+                      clipPath="url(#app-pencil_svg__q)"
+                      data-name="Group 7517"
+                    >
+                      <path
+                        fill="url(#app-pencil_svg__r)"
+                        d="M0 0h42.489v29.138H0z"
+                        data-name="Rectangle 2922"
+                        transform="rotate(-69.7 44.95 .22)"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7524"
+              opacity={0.4}
+              style={{
+                mixBlendMode: "multiply",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7523">
+                <g clipPath="url(#app-pencil_svg__s)" data-name="Group 7522">
+                  <path
+                    fill="#840855"
+                    d="M32.03 50.695s3.726-.89 5.606-.323 1.956 4.363 1.956 4.363a22 22 0 0 0-2.868-3.47c-.95-.683-4.921.034-4.921.034Z"
+                    data-name="Path 5140"
+                  />
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7529"
+              opacity={0.4}
+              style={{
+                mixBlendMode: "multiply",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7528">
+                <g clipPath="url(#app-pencil_svg__t)" data-name="Group 7527">
+                  <g data-name="Group 7526">
+                    <g
+                      clipPath="url(#app-pencil_svg__u)"
+                      data-name="Group 7525"
+                    >
+                      <path
+                        fill="url(#app-pencil_svg__v)"
+                        d="M0 0h26.599v23.694H0z"
+                        data-name="Rectangle 2925"
+                        transform="rotate(-19.5 33.252 -94.252)"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g
+              data-name="Group 7532"
+              opacity={0.61}
+              style={{
+                mixBlendMode: "soft-light",
+                isolation: "isolate",
+              }}
+            >
+              <g data-name="Group 7531">
+                <g clipPath="url(#app-pencil_svg__w)" data-name="Group 7530">
+                  <path
+                    fill="#fff6bb"
+                    d="M36.41 10.7 16.622 41.682h3.716S19.14 36.615 36.41 10.7"
+                    data-name="Path 5142"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default SvgAppPencil;
