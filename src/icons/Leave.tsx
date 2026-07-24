97957f466fb6eca71fba64298790b0603747538c
Ben Afonso <ben@nightzookeeper.com>
2026-07-24 17:08:45 +0200

    chore: add new icons

--- src/icons/Leave.tsx
+++ src/icons/Leave.tsx
@@ -5,110 +5,112 @@ const SvgLeave = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    viewBox="0 0 70 70"
+    viewBox="0 0 70.018 70"
     {...props}
   >
     <defs>
       <clipPath id="leave_svg__a">
-        <path fill="none" d="M0 0h70v70H0z" data-name="Rectangle 5269" />
-      </clipPath>
-      <clipPath id="leave_svg__b">
-        <path
-          fill="none"
-          d="M.207 13.253H70V58.61H.207z"
-          data-name="Rectangle 5266"
-        />
-      </clipPath>
-      <clipPath id="leave_svg__c">
-        <path
-          fill="none"
-          d="M.208 13.253H70v45.356H.208z"
-          data-name="Rectangle 5265"
-        />
-      </clipPath>
-      <clipPath id="leave_svg__d">
-        <path
-          fill="none"
-          d="M5.697 18.084H60.12v25.661H5.697z"
-          data-name="Rectangle 5267"
-        />
+        <path fill="none" d="M.2 13.3H70v45.4H.2z" data-name="Rectangle 6083" />
       </clipPath>
     </defs>
-    <g clipPath="url(#leave_svg__a)" data-name="Group 14819">
-      <g data-name="Group 14818">
-        <g clipPath="url(#leave_svg__a)" data-name="Group 14817">
-          <g
-            data-name="Group 14813"
-            opacity={0.3}
-            style={{
-              mixBlendMode: "normal",
-              isolation: "isolate",
-            }}
-          >
-            <g data-name="Group 14812">
-              <g clipPath="url(#leave_svg__b)" data-name="Group 14811">
-                <g data-name="Group 14810">
-                  <g clipPath="url(#leave_svg__c)" data-name="Group 14809">
-                    <path
-                      d="M69.689 32.818c-.673-2-20.856-18.729-23.638-19.522-2.023-.577-1.545 4.9-3.1 5.348S.438 15.638.221 18.083c-.027.314-.014 1.108.038 2.25a.2.2 0 0 0-.031.087C.011 22.87 2.105 58.357 5 58.57c.28.243 4.419-.659 10.366-2.059 3.725-.87 8.008-1.907 12.109-2.892h.01c11.766-2.812 16.437-3.491 16.437-3.491 2 .44 2.907 5.778 4.687 5.775a1.4 1.4 0 0 0 .5-.169c3.591-1.776 21.5-16.88 20.869-18.764-.075-.224.229-3.562-.292-4.152"
-                      data-name="Path 12679"
-                    />
-                  </g>
-                </g>
-              </g>
-            </g>
+    <g data-name="leave">
+      <path fill="none" d="M0 0h70v70H0z" data-name="Rectangle 6080" />
+      <path fill="none" d="M0 0h70v70H0z" data-name="Rectangle 6081" />
+      <g data-name="Group 16875" opacity={0.3}>
+        <g clipPath="url(#leave_svg__a)" data-name="Group 16874">
+          <g clipPath="url(#leave_svg__a)" data-name="Group 16873">
+            <path d="M69.7 32.8c-.7-2-20.9-18.7-23.6-19.5-2-.6-1.5 4.9-3.1 5.3S.4 15.6.2 18.1v2.2C0 22.8 2.1 58.2 5 58.4c.3.2 4.4-.7 10.4-2.1 3.7-.9 8-1.9 12.1-2.9 11.8-2.8 16.4-3.5 16.4-3.5 2 .4 2.9 5.8 4.7 5.8a.55.55 0 0 0 .5-.2c3.6-1.8 21.5-16.9 20.9-18.8 0-.2.2-3.6-.3-4.2" />
           </g>
+        </g>
+      </g>
+      <path
+        fill="#bdbdbd"
+        d="M46.1 11.7c2.5.7 19.5 12.5 23.6 17.2.5.6.2 3.9.3 4.2.7 2-19.6 18.9-21.4 18.9s-2.7-5.3-4.7-5.8-36 8.6-38.9 8.4S0 18.9.2 16.5s41.2 1 42.7.6 1.1-5.9 3.1-5.3"
+      />
+      <path
+        fill="#fff"
+        d="M46.1 9.3c2.8.8 23 17.5 23.6 19.5S50.4 49.5 48.6 49.6s-2.7-5.3-4.7-5.8-35.8 4.8-38.7 4.6S0 16.6.2 14.1c.2-2.4 41.2 1 42.7.6S44 8.8 46 9.4"
+      />
+      <path
+        fill="#8a8a8a"
+        d="M43.9 46.2c.8-.2 0-2.4-.8-2.3s-22.8 3-30 3.8-8.5 6.6-8 7 10.7-2.1 22.5-5C39.4 46.9 44 46.2 44 46.2M69.7 33.8s-1-2.3-6.6 3.2-13.2 11.7-13.8 12.6a2.8 2.8 0 0 0-.2 2.2s12.9-8.9 20.6-18"
+      />
+      <path
+        fill="none"
+        d="M5.7 18.1h54.4v25.7H5.7z"
+        data-name="Rectangle 6084"
+      />
+      <g
+        data-name="Leave-2"
+        style={{
+          mixBlendMode: "normal",
+          isolation: "isolate",
+        }}
+      >
+        <g
+          data-name="Group 16876"
+          style={{
+            mixBlendMode: "normal",
+            isolation: "isolate",
+          }}
+        >
           <path
-            fill="#bdbdbd"
-            d="M46.057 11.655c2.47.7 19.518 12.5 23.632 17.186.518.59.214 3.928.292 4.152.67 2-19.593 18.93-21.373 18.934s-2.683-5.334-4.688-5.775S7.892 54.813 5 54.6.011 18.9.228 16.45s41.171 1.007 42.727.558 1.08-5.925 3.1-5.348"
-            data-name="Path 12680"
+            fill="#662d80"
+            d="m6.7 25.1 4.4-.5.8 7.9 3.8-.4.4 3.4-8.2.9z"
+            data-name="Path 13615"
           />
+        </g>
+        <g
+          data-name="Group 16877"
+          style={{
+            mixBlendMode: "normal",
+            isolation: "isolate",
+          }}
+        >
           <path
-            fill="#fff"
-            d="M46.051 9.319c2.782.793 22.966 17.522 23.638 19.522S50.382 49.584 48.6 49.591s-2.683-5.334-4.688-5.775S8.1 48.581 5.2 48.366.006 16.556.222 14.108s41.171 1.008 42.727.558 1.08-5.925 3.1-5.348"
-            data-name="Path 12681"
+            fill="#662d80"
+            d="M21.7 31.9h-.8c0 .6.4.8 1 .8h.5a5.6 5.6 0 0 0 1.5-.4 3.27 3.27 0 0 0 1.5-1.1l.7 2.2a7.85 7.85 0 0 1-4.6 1.9 5.36 5.36 0 0 1-3.3-.7 4.3 4.3 0 0 1-1.8-3.1 4.86 4.86 0 0 1 .5-2.9 3.9 3.9 0 0 1 1.9-1.9 6.2 6.2 0 0 1 2.7-.9 4.53 4.53 0 0 1 3 .5 2.645 2.645 0 0 1 .2 4.3 4.7 4.7 0 0 1-3.1 1.3Zm.6-3.4a.576.576 0 0 0-.6-.6c-.4 0-.7.3-.8.8v1.6l1.5-.5v-1.3Z"
+            data-name="Path 13616"
           />
+        </g>
+        <g
+          data-name="Group 16878"
+          style={{
+            mixBlendMode: "normal",
+            isolation: "isolate",
+          }}
+        >
           <path
-            fill="#8a8a8a"
-            d="M43.919 46.153c.834-.225.1-2.414-.844-2.35s-22.843 2.992-30.028 3.773S4.55 54.209 5 54.6s10.72-2.142 22.485-4.953 16.436-3.494 16.436-3.494"
-            data-name="Path 12682"
+            fill="#662d80"
+            d="M28.5 33.5a2.94 2.94 0 0 1-1-1.2 3.37 3.37 0 0 1-.5-1.7V29a6.4 6.4 0 0 1 1-2.4l.9-.9a4.65 4.65 0 0 1 2.3-.9 3.34 3.34 0 0 1 2.2.6c.2.2.4.3.6.5l.9-1.2 2.3-.2.9 8.8-2.3.2-1.2-1.4a4.24 4.24 0 0 1-3.3 2.1 7 7 0 0 1-1.4-.1 3.5 3.5 0 0 1-1.4-.6m5.1-6c-.2 0-.4-.1-.6 0a1.08 1.08 0 0 0-.7.3c-.2.2-.5.4-.6.7a2.5 2.5 0 0 0-.5 1.8.79.79 0 0 0 .5.8 1.6 1.6 0 0 0 1.1-.2A6.2 6.2 0 0 0 34 30l-.3-2.5Z"
+            data-name="Path 13617"
           />
+        </g>
+        <g
+          data-name="Group 16879"
+          style={{
+            mixBlendMode: "normal",
+            isolation: "isolate",
+          }}
+        >
           <path
-            fill="#8a8a8a"
-            d="M69.718 33.806s-.968-2.284-6.625 3.177-13.16 11.658-13.768 12.606a2.86 2.86 0 0 0-.223 2.17s12.867-8.851 20.616-17.953"
-            data-name="Path 12683"
+            fill="#662d80"
+            d="m43.9 26.3.9-2.7 3.4-.4-3.4 9.2c0 .2-.2.3-.5.3s-.5 0-.6-.2l-5.5-8.3 4.1-.4z"
+            data-name="Path 13618"
           />
-          <g
-            data-name="Group 14816"
-            style={{
-              mixBlendMode: "normal",
-              isolation: "isolate",
-            }}
-          >
-            <g data-name="Group 14815">
-              <g clipPath="url(#leave_svg__d)" data-name="Group 14814">
-                <text
-                  fill="#662d80"
-                  data-name="Leave"
-                  fontFamily="RammettoOne-Regular, Rammetto One"
-                  fontSize={14.583}
-                  transform="rotate(-6.024 350.447 -48.834)"
-                >
-                  <tspan x={0} y={0}>
-                    {"L"}
-                  </tspan>
-                  <tspan y={0} letterSpacing="-.021em">
-                    {"e"}
-                  </tspan>
-                  <tspan y={0}>{"a"}</tspan>
-                  <tspan y={0} letterSpacing="-.043em">
-                    {"v"}
-                  </tspan>
-                  <tspan y={0}>{"e"}</tspan>
-                </text>
-              </g>
-            </g>
-          </g>
+        </g>
+        <g
+          data-name="Group 16880"
+          style={{
+            mixBlendMode: "normal",
+            isolation: "isolate",
+          }}
+        >
+          <path
+            fill="#662d80"
+            d="M54.1 28.5h-.8c0 .6.4.8 1 .8h.5a5.6 5.6 0 0 0 1.5-.4 3.27 3.27 0 0 0 1.5-1.1l.7 2.2a7.85 7.85 0 0 1-4.6 1.9 5.36 5.36 0 0 1-3.3-.7 4.3 4.3 0 0 1-1.8-3.1 4.86 4.86 0 0 1 .5-2.9 3.9 3.9 0 0 1 1.9-1.9 6.2 6.2 0 0 1 2.7-.9 4.53 4.53 0 0 1 3 .5 2.645 2.645 0 0 1 .2 4.3 4.7 4.7 0 0 1-3.1 1.3Zm.6-3.4a.576.576 0 0 0-.6-.6c-.4 0-.7.3-.8.8v1.6l1.5-.5v-1.3Z"
+            data-name="Path 13619"
+          />
         </g>
       </g>
     </g>
