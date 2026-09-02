f3768bf9b902ca52db8723a25f5facd7f66f3a26
Ben Afonso <ben@nightzookeeper.com>
2026-09-02 13:55:49 +0200

    chore: add new icons

---
+++ src/icons/Smile.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgSmile = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 69.549 69.55"
+    {...props}
+  >
+    <defs>
+      <clipPath id="smile_svg__a">
+        <path
+          fill="none"
+          d="M0 0h69.549v69.549H0z"
+          data-name="Rectangle 6108"
+        />
+      </clipPath>
+      <clipPath id="smile_svg__b">
+        <path
+          fill="none"
+          d="M0-.001h69.55v69.55H0z"
+          data-name="Rectangle 6107"
+        />
+      </clipPath>
+      <clipPath id="smile_svg__c">
+        <path
+          fill="none"
+          d="M8.995 9.406h51.559v51.559H8.995z"
+          data-name="Rectangle 6106"
+        />
+      </clipPath>
+    </defs>
+    <g clipPath="url(#smile_svg__a)" data-name="Group 16919">
+      <g data-name="Group 16918">
+        <g clipPath="url(#smile_svg__b)" data-name="Group 16917">
+          <g data-name="Group 16916" opacity={0.3}>
+            <g data-name="Group 16915">
+              <g clipPath="url(#smile_svg__c)" data-name="Group 16914">
+                <path
+                  d="M60.554 35.186a25.78 25.78 0 1 1-25.78-25.78 25.78 25.78 0 0 1 25.78 25.78"
+                  data-name="Path 13673"
+                />
+              </g>
+            </g>
+          </g>
+          <path
+            fill="#ffc83e"
+            d="M60.554 32.993a25.78 25.78 0 1 1-25.78-25.78 25.78 25.78 0 0 1 25.78 25.78"
+            data-name="Path 13674"
+          />
+          <path
+            fill="#503834"
+            d="M34.775 42.138a25.7 25.7 0 0 1-10.591-2.278 12.85 12.85 0 0 0 21.183 0 25.7 25.7 0 0 1-10.592 2.278"
+            data-name="Path 13675"
+          />
+          <path
+            fill="#f8b33b"
+            d="M58.565 23.05A25.782 25.782 0 0 1 9.47 37.915 25.781 25.781 0 1 0 58.565 23.05"
+            data-name="Path 13676"
+          />
+          <path
+            fill="#ffd977"
+            d="M27.771 12.346c.71 1.113-.586 3.208-2.894 4.679s-4.753 1.762-5.462.649.586-3.208 2.893-4.679 4.753-1.762 5.463-.649"
+            data-name="Path 13677"
+          />
+          <path
+            fill="#503834"
+            d="M28.446 29.905a3.087 3.087 0 1 1-3.087-3.086 3.087 3.087 0 0 1 3.087 3.086"
+            data-name="Path 13678"
+          />
+          <path
+            fill="#503834"
+            d="M46.97 29.905a3.088 3.088 0 1 1-3.087-3.086 3.087 3.087 0 0 1 3.087 3.086"
+            data-name="Path 13679"
+          />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default SvgSmile;
